771d32824d6395e5db775c8873888b14cdeed675
Marcos Vinícius <marcos.vinicius124816@gmail.com>
2022-12-09 15:50:52 -0300

    init

---
+++ trab/block.py
@@ -0,0 +1,9 @@
+import torch
+from torch import nn
+from torchvision import transforms
+import functools
+from IPython.display import Image as ipy_image
+from PIL import Image
+import numpy as np
+
+#def (self, dim, padding_type, norm_layer, use_dropout, use_bias):
